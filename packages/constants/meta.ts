b85624587d55aea6fb4f192fa86b2ec2e35194d3
Tomas Tannergren <tomas@bruce.app>
2023-04-29 19:25:26 +0200

    feat: add some metadata

--- packages/constants/meta.ts
+++ packages/constants/meta.ts
@@ -1,4 +1,7 @@
 export const META = {
   TITLE: 'epoxide',
   DESCRIPTION: 'Library and landing page for epoxide.',
+  SITE_NAME: 'epoxide',
+  URL: 'https://epoxide.se',
+  LOCALE: 'en-US',
 }
--- packages/next/src/app/layout.tsx
+++ packages/next/src/app/layout.tsx
@@ -1,10 +1,19 @@
 import './globals.scss'
 import { Raleway } from 'next/font/google'
 import { META } from '@epoxide/web-constants'
+import type { Metadata } from 'next'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
+  openGraph: {
+    title: META.TITLE,
+    description: META.DESCRIPTION,
+    siteName: META.SITE_NAME,
+    url: META.URL,
+    locale: META.LOCALE,
+    type: 'website',
+  },
   title: META.TITLE,
   description: META.DESCRIPTION,
 }

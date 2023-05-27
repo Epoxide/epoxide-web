c2fef934be465ad574e852e23b77df00f56846e1
Tomas Tannergren <tomas@bruce.app>
2023-05-27 19:38:29 +0200

    feat(next): open all links in a new tab

--- packages/next/src/app/page.tsx
+++ packages/next/src/app/page.tsx
@@ -7,9 +7,13 @@ export default function Home() {
     <main className={styles.main}>
       <h1 className={styles.title}>{HOME.TITLE}</h1>
       <section className={styles.links}>
-        <Link href={HOME.LINKS.R2AT.URL}>{HOME.LINKS.R2AT.TEXT}</Link>
-        <Link href={HOME.LINKS.TOTTES_KZ.URL}>{HOME.LINKS.TOTTES_KZ.TEXT}</Link>
-        <Link href={HOME.LINKS.KZ_REPLAY_VIEWER.URL}>
+        <Link href={HOME.LINKS.R2AT.URL} target="_blank">
+          {HOME.LINKS.R2AT.TEXT}
+        </Link>
+        <Link href={HOME.LINKS.TOTTES_KZ.URL} target="_blank">
+          {HOME.LINKS.TOTTES_KZ.TEXT}
+        </Link>
+        <Link href={HOME.LINKS.KZ_REPLAY_VIEWER.URL} target="_blank">
           {HOME.LINKS.KZ_REPLAY_VIEWER.TEXT}
         </Link>
       </section>

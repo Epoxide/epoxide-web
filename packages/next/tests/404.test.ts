8666240705f5f5d75350e400517f2b7860ece867
Epoxide <epoxide@epoxide.se>
2023-11-18 22:07:15 +0100

    test(next): add 404 test

---
+++ packages/next/tests/404.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test'
+import { NOT_FOUND } from '@epoxide/web-constants'
+
+test('has h1 with not found text', async ({ page }) => {
+  await page.goto('/non-existing-route')
+  await expect(page.locator('h1')).toHaveText(NOT_FOUND.TITLE)
+})
+
+test('has link to home', async ({ page }) => {
+  await page.goto('/non-existing-route')
+  await page.getByText(NOT_FOUND.GO_BACK).click()
+  await expect(page).toHaveURL('/')
+})

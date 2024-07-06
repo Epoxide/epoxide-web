2f8fd5de12f6f73252b2637e79c28a000d45fd6a
Epoxide <epoxide@epoxide.se>
2024-07-06 19:34:44 +0200

    feat(nuxt): add html lang attribute

--- packages/nuxt/nuxt.config.ts
+++ packages/nuxt/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+    },
+  },
   devtools: { enabled: true },
   routeRules: {
     '/R2AT': {

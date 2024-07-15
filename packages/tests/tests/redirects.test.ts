ebb1cc5aef17c48365f6558a45958033fbfbb8b9
Epoxide <epoxide@epoxide.se>
2024-07-16 01:04:13 +0200

    test: wait for url before checking after redirect

--- packages/tests/tests/redirects.test.ts
+++ packages/tests/tests/redirects.test.ts
@@ -3,9 +3,12 @@ import { HOME, REDIRECTS } from '@epoxide/web-constants'
 
 test('redirects to old', async ({ page }) => {
   await page.goto(REDIRECTS.R2AT.PATH)
+  await page.waitForURL(`${HOME.LINKS.R2AT.URL}/`)
   await expect(page.url()).toBe(`${HOME.LINKS.R2AT.URL}/`)
   await page.goto(REDIRECTS.TOTTES_KZ.PATH)
+  await page.waitForURL(`${HOME.LINKS.TOTTES_KZ.URL}/`)
   await expect(page.url()).toBe(`${HOME.LINKS.TOTTES_KZ.URL}/`)
   await page.goto(REDIRECTS.KZ_REPLAY_VIEWER.PATH)
+  await page.waitForURL(`${HOME.LINKS.KZ_REPLAY_VIEWER.URL}/`)
   await expect(page.url()).toBe(`${HOME.LINKS.KZ_REPLAY_VIEWER.URL}/`)
 })

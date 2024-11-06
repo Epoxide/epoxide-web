efa663c3cd8b491ae85853189437b05eba3adcb5
Epoxide <epoxide@epoxide.se>
2024-11-06 16:54:41 +0100

    chore: update lint staged to esm and use correct syntax

--- .lintstagedrc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '**/*.{js,jsx,cjs,ts,tsx,astro,vue,svelte}': 'npm run lint',
-  '**/*.{astro,svelte}': 'npm run check',
-}
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+export default {
+  '**/*.{js,jsx,cjs,ts,tsx,astro,vue,svelte}':
+    "eslint --rule 'no-console: error' --rule 'prettier/prettier: error'",
+}

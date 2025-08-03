8c0311104187b1325d4a01616d9f5068bef1475f
Tomas Tannergren <Epoxide@users.noreply.github.com>
2025-08-03 21:58:12 +0200

    ci: change playwright testing (#2)
    * ci: change playwright testing
    
    * ci: use next test results path
    
    * test: update snapshots
    
    ---------
    
    Co-authored-by: Epoxide <11177412+Epoxide@users.noreply.github.com>

--- packages/tests/tests/home.test.ts
+++ packages/tests/tests/home.test.ts
@@ -28,9 +28,6 @@ test('matches desktop snapshot', async ({ page }) => {
   await page.setViewportSize({ width: 1920, height: 1080 })
   await expect(page).toHaveScreenshot(
     '../snapshots/home.test.ts.matches-desktop-snapshot.png',
-    {
-      maxDiffPixelRatio: 0.02,
-    },
   )
 })
 
@@ -39,8 +36,5 @@ test('matches mobile snapshot', async ({ page }) => {
   await page.setViewportSize({ width: 393, height: 852 })
   await expect(page).toHaveScreenshot(
     '../snapshots/home.test.ts.matches-mobile-snapshot.png',
-    {
-      maxDiffPixelRatio: 0.02,
-    },
   )
 })

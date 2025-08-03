686e664f44cbcee3a4ef8532c1b59106001318a5
Epoxide <epoxide@epoxide.se>
2025-08-03 19:29:02 +0200

    chore(nuxt): update dependencies

--- packages/nuxt/.eslintrc.cjs
+++ packages/nuxt/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['../../.eslintrc.cjs', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: ['../../.eslintrc.cjs', 'plugin:vue/recommended', 'prettier'],
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
